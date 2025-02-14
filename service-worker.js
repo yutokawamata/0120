3722ad6366d779f5631efa029391160fbf7ac7a0
yutokawamata <y.kwmt16223@gmail.com>
2025-02-14 03:33:11 +0000

    Updates

---
+++ service-worker.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'kanji-challenge-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/static/js/main.bundle.js',
+  '/static/css/main.css',
+  '/manifest.json',
+  '/favicon.ico',
+  '/logo192.png',
+  '/logo512.png'
+];
+
+// インストール時の処理
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// フェッチ時の処理
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// アクティベート時の処理
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+}); 
